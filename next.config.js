ba3124672ddd449b104e6f732622f69c1d11f115
colinc1123 <cammackcolin92@gmail.com>
2026-01-19 12:48:35 -0500

    Refactor outputFileTracingIncludes configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      "/blog": ["./app/blog/posts/**/*"],
-      "/blog/[slug]": ["./app/blog/posts/**/*"],
-    },
+  outputFileTracingIncludes: {
+    "/blog": ["./app/blog/posts/**/*"],
+    "/blog/[slug]": ["./app/blog/posts/**/*"],
   },
 };
 
