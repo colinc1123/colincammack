c07c2e6b28215c07a9fd5d98705a0d0fa9ac3bf1
colinc1123 <cammackcolin92@gmail.com>
2026-01-19 13:58:45 -0500

    Update footer links and text

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -46,10 +46,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://github.com/colinc1123"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">email</p>
           </a>
         </li>
       </ul>
