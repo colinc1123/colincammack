1b63ad67949c8290fafca20e064aa66fea78e6e7
colinc1123 <cammackcolin92@gmail.com>
2026-01-21 23:53:01 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Colin Cammack',
+    template: '%s | Colin Cammack',
   },
   description: 'This is my portfolio.',
   openGraph: {
