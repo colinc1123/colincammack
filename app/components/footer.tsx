cc60c8fed235a5c2f3f843a9125ebf003b0407de
colinc1123 <cammackcolin92@gmail.com>
2026-01-21 23:47:18 -0500

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,10 +24,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
+            href="https://www.linkedin.com/in/colin-cammack-a4b21a225/"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
+            <p className="ml-2 h-7">linkedin</p>
           </a>
         </li>
         <li>
@@ -35,7 +35,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="hhttps://github.com/colinc1123"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
@@ -46,7 +46,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/colinc1123"
+            href="mailto:colincammack@icloud.com"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">email</p>
