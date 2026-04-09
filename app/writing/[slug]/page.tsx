47654b11d21a1ccdc8fb21bbf72f76b4ae551929
colinc1123 <cammackcolin92@gmail.com>
2026-04-09 03:22:45 +0000

    testsss

--- app/writing/[slug]/page.tsx
+++ app/writing/[slug]/page.tsx
@@ -73,7 +73,7 @@ export default async function WritingPost({ params }) {
           }),
         }}
       />
-      <h1 className="title font-semibold text-2xl tracking-tighter">
+      <h1 className="title font-semibold text-4xl tracking-tighter">
         {post.metadata.title}
       </h1>
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
